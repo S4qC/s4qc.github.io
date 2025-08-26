5d95e2c6c3a70594e08aa1e7bfba1eaf6434c5a9
S4qC <mohammadsaadbinzahoor635076@gmail.com>
2025-08-26 14:55:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// Force dark mode on first load
-if (!localStorage.getItem("theme")) {
-  localStorage.setItem("theme", "dark");
-  document.documentElement.setAttribute("data-theme", "dark");
-} else {
-  document.documentElement.setAttribute("data-theme", localStorage.getItem("theme"));
-}
+
